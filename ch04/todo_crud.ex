5bc69182e4ea8c21f9132e09f92269050595cfd6
Kernelgarden <kernelgarden@gmail.com>
2018-10-08 16:13:23 +0900

    progress in todolist_crud

---
+++ ch04/todo_crud.ex
@@ -0,0 +1,47 @@
+defmodule TodoList do
+
+  defstruct auto_id: 1, entries: %{}
+
+  def new(), do: %TodoList{}
+
+  def add_entry(todo_list, entry) do
+    # 새로운 entry에 id 부여
+    entry = Map.put(entry, :id, todo_list.auto_id)
+
+    # 새로운 엔트리 맵 생성
+    new_entries = Map.put(todo_list.entries,
+      todo_list.auto_id,
+      entry)
+
+    # struct 업데이트
+    %TodoList{todo_list |
+      entries: new_entries,
+      auto_id: todo_list.auto_id + 1}
+  end
+
+  def entries(todo_list, date) do
+    # iterate whole map once
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        IO.puts("cannot find id - #{entry_id} !!!")
+        todo_list
+
+      {:ok, old_entry} ->
+        # pin operator로 id를 고정시킨다.
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, entry_id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+end

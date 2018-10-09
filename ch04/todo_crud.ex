badf58593f9343a7b61987c2f4e6f9c153121e8a
Kernelgarden <kernelgarden@gmail.com>
2018-10-09 13:33:16 +0900

    finished ch04

--- ch04/todo_crud.ex
+++ ch04/todo_crud.ex
@@ -89,3 +89,15 @@ defmodule TodoList.CsvImporter do
     %{date: date, title: title}
   end
 end
+
+defimpl Collectable, for: TodoList do
+  def into(original) do
+    {original, &into_callback/2}
+  end
+
+  defp into_callback(todo_list, {:cont, entry}) do
+    TodoList.add_entry(todo_list, entry)
+  end
+  defp into_callback(todo_list, :done), do: todo_list
+  defp into_callback(todo_list, :halt), do: :ok
+end

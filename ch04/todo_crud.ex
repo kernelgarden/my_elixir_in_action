2f67344e8fe2aa4f6eb0b29ecab831d2986052be
Kernelgarden <kernelgarden@gmail.com>
2018-10-09 11:59:52 +0900

    finished todo csv importer practice

--- ch04/todo_crud.ex
+++ ch04/todo_crud.ex
@@ -56,3 +56,36 @@ defmodule TodoList do
     %TodoList{todo_list | entries: new_entries}
   end
 end
+
+defmodule TodoList.CsvImporter do
+
+  def get_test_path(), do: Path.expand('dummy.csv') |> Path.absname()
+
+  def import(path) do
+    read_csv(path)
+  end
+
+  defp read_csv(path) do
+    entities = File.stream!(path)
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> Enum.map(fn line -> transform_to_entry(parse_line(line)) end)
+
+    TodoList.new(entities)
+  end
+
+  defp parse_line(line) do
+    [date, title] = String.split(line, ",")
+    {parse_date(date), title}
+  end
+
+  defp parse_date(date_string) do
+    [year, month, day] = String.split(date_string, "/")
+    {String.to_integer(year), String.to_integer(month), String.to_integer(day)}
+  end
+
+  defp transform_to_entry(raw) do
+    {{year, month, day}, title} = raw
+    {:ok, date} = Date.new(year, month, day)
+    %{date: date, title: title}
+  end
+end
